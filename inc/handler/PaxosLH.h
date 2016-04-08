64c3703a0c2acb4bc1f046ef1d2c73d4831ec330
Loïc Glémot <loicglemot@gmail.com>
2016-04-08 18:21:24 +0200

    Create PaxosLH.h

---
+++ inc/handler/PaxosLH.h
@@ -0,0 +1,24 @@
+/*
+ * PaxosLH.h
+ *
+ *  Created on: Apr 7, 2016
+ *      Author: gll
+ */
+
+#ifndef PAXOSLH_H_
+#define PAXOSLH_H_
+
+#include <stdint.h>
+#include <string>
+#include <boost/asio.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/bind.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/system/error_code.hpp>
+
+namespace paxos
+{
+
+}/* namespace paxos */
+
+#endif /* PAXOSLH_H_ */
